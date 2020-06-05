e36149370e68648a1d215e6c742a03938cdab831
Ali Rajaii <rajaii_a@yahoo.com>
2020-06-05 13:16:13 -0400

    initial project setup and began server and db config

---
+++ api/server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const express = require('express');
+const server = express();
+
+
+
+
+
+module.exports = server;
---
+++ knexfile.js
@@ -0,0 +1,49 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: "pg", // pg is the database library for postgreSQL on knexjs
+    connection: {
+      host: "127.0.0.1", // Your local host IP
+      user: "rosin", // Your postgres user name
+      password: process.env.PGPASSWORD, // Your postgres user password
+      database: "maniped", // Your database name
+      // port: 5432???
+    }
+  },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
+
