7216276264f68cde40d2844ce30fb49cf50bc918
Ali Rajaii <rajaii_a@yahoo.com>
2020-07-20 21:26:01 -0400

    nearbyProvidersRouter tested and working

--- api/endpoints/providers/nearbyProvidersRouter.js
+++ api/endpoints/providers/nearbyProvidersRouter.js
@@ -8,60 +8,45 @@ const Providers = require('./providersHelpers.js');
 
 
 router.get('/', (req, res) => {
-    console.log('going in')
     const { zipCode, distance } = req.body;
     
-    const closeZips = {}
-    let zips;
+    const closeZips = {};
+    
   
             const options = {
               uri: `https://www.zipcodeapi.com/rest/${process.env.ZIPCODEKEY}/radius.json/${zipCode}/${distance}/miles`,
               headers: {
                   'User-Agent': 'Request-Promise'
               },
+              respolveWithFullResponse: true,
               json: true // Automatically parses the JSON string in the response
             };
             rp.get(options)
-            .then(res => {
-            //   res["zip_codes"].map(z => {
-            //   closeZips[z.zip_code] = z.city;
-            
-            
-            // })
-            zips = res
-              
-            })
-            .catch(err => {
-              console.log(err)
-            })
-            console.log(`ZIPS: ${zips}`)
-
-            console.log(`closeZips: ${JSON.stringify(closeZips)}`)
-            Providers.find()
+            .then(response => {
+               response["zip_codes"].map(z => {
+               closeZips[z.zip_code] = z.city;
+             })
+             Providers.find()
             .then(providers => {
-              console.log(`providers: ${JSON.stringify(providers)}`);
-              let localProviders = providers.filter(p => p["zipcode"] === zipCode) 
+              let localProviders = providers.filter(p => p["zipcode"] === zipCode); 
 
-              for (let i = 0; i < providers.length; i++) {
-                
-                for (let key in closeZips) {
-                    
-                    if (providers[i].zipcode === key) {
-                        
-                        localProviders.push(providers[i])
-                        
+              for (let i = 0; i < providers.length; i++) { 
+                    if (closeZips[providers[i].zipcode] !== undefined ) {
+                        localProviders.push(providers[i]);
                     }
                 }
-              }
               res.status(200).json(localProviders);
-              console.log(`localproviders: ${JSON.stringify(localProviders)}`)
               })
               
             .catch(err => {
                 console.log(err)
               res.status(500).json(err)
             })       
-  
+              
+            })
+            .catch(err => {
+              res.status(500).json(err);
+            })
             
     });
 
