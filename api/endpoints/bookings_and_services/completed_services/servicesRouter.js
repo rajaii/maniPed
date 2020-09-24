d36cd4c0029dbc940266a7bc2dda57bebbad7d54
Ali Rajaii <rajaii_a@yahoo.com>
2020-09-24 17:25:26 -0400

    documented the breakpoints in notes.txt in case do not finish the debugging this week

--- api/endpoints/bookings_and_services/completed_services/servicesRouter.js
+++ api/endpoints/bookings_and_services/completed_services/servicesRouter.js
@@ -97,9 +97,8 @@ router.post('/', (req, res) => {
         .then(async booking => {
             Users.findById(user_id)
             .then(async user => {
-              console.log('stripe sucty id', user)
               const paymentMethods = await stripe.paymentMethods.list({
-                customer: user[0].stripe_custyid,
+                customer: user.stripe_custyid,
                 type: 'card',
               });
               console.log("in here", paymentMethods.id)
@@ -109,7 +108,7 @@ router.post('/', (req, res) => {
                 const paymentIntent = await stripe.paymentIntents.create({
                   amount: service[0].amount_billed * 100,
                   currency: 'usd',
-                  customer: user[0].stripe_custyid,
+                  customer: user.stripe_custyid,
                   payment_method: paymentMethods.id,
                   off_session: true,
                   confirm: true,
