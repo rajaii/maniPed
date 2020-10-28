5692b3375bd73f7ca05c209a19daeff59bbba124
Ali Rajaii <rajaii_a@yahoo.com>
2020-10-28 17:30:23 -0400

    hair_services router and helpers pupluated and tested

--- api/server.js
+++ api/server.js
@@ -15,7 +15,7 @@ const preAdminRouter = require('./endpoints/admin/pre_admin/preAdminRouter.js');
 const bookingsRouter = require('./endpoints/bookings_and_services/future_bookings/bookingsRouter.js');
 const servicesRouter = require('./endpoints/bookings_and_services/completed_services/servicesRouter.js');
 const nailsServicesRouter = require('./endpoints/bookings_and_services/nails_services/nails_servicesRouter.js');
-// const hairServicesRouter = require('./endpoints/bookings_and_services/hair_services/hair_servicesRouter.js');
+const hairServicesRouter = require('./endpoints/bookings_and_services/hair_services/hair_servicesRouter.js');
 // const massageServicesRouter = require('./endpoints/bookings_and_services/massage_services/massage_servicesRouter.js');
 
 const user_ratingsRouter = require('./endpoints/ratings/user_ratings/user_ratingsRouter.js');
@@ -49,7 +49,7 @@ server.use('/api/preadmin', restricted, preAdminRouter);
 server.use('/api/future_bookings', /*restricted,*/ bookingsRouter);
 server.use('/api/services', /*restricted,*/ servicesRouter);
 server.use('/api/nails_servicesrouter', nailsServicesRouter);
-// server.use('/api/hair_servicesrouter', hairServicesRouter);
+server.use('/api/hair_servicesrouter', hairServicesRouter);
 // server.use('/api/massage_servicesrouter', massageServicesRouter);
 
 server.use('/api/user_ratings', /*restricted,*/ user_ratingsRouter);
